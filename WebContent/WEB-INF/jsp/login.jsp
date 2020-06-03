47d53aeb1f66ced9cffb4528095494a880c36ab4
edu03 <edu03@EDUPC03.n00d01.kis.co.jp>
2020-06-03 15:43:02 +0900

    login page

---
+++ WebContent/WEB-INF/jsp/login.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+
+<link href="./css/loginstyle.css" rel="stylesheet">
+<title>ログイン</title>
+</head>
+<body>
+<div class="log-form">
+  <h2>Login to your account</h2>
+  <form>
+    <input type="text" title="username" placeholder="username" />
+    <input type="password" title="username" placeholder="password" />
+    <button type="submit" class="btn">Login</button>
+    <a class="forgot" href="#">Forgot Username?</a>
+  </form>
+</div><!--end log form -->
+</body>
+</html>
--- src/com/tdn/servlet/LoginServlet.java
+++ src/com/tdn/servlet/LoginServlet.java
@@ -2,11 +2,16 @@ package com.tdn.servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.tdn.model.Admin;
+import com.tdn.model.AdminDAO;
 
 /**
  * Servlet implementation class LoginServlet
@@ -27,16 +32,38 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session=request.getSession();
+		String error=(String)session.getAttribute("error");
+		if(error !=null) {
+			request.setAttribute("mes", error);
+			session.removeAttribute(error);
+
+		}
+		RequestDispatcher dispatcher=request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		request.setCharacterEncoding("UTF-8");
+		String name=request.getParameter("adminId");
+		String pass=request.getParameter("password");
 
+		AdminDAO dao=new AdminDAO();
+		Admin a =dao.login(name, pass);
+
+		if(a !=null) {
+			HttpSession session=request.getSession();
+			session.setAttribute("admin", a);
+
+			response.sendRedirect("top");
+		}
+		else {
+			HttpSession session=request.getSession();
+			session.setAttribute("mes", "パスワードが違います");
+			response.sendRedirect("login");
+	}
+	}
 }
