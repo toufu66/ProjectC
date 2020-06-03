287c1eca84b7a89d9409740362a9f18c2239199e
edu04 <edu04@EDUPC04.n00d01.kis.co.jp>
2020-06-03 13:34:47 +0900

    admin.java

--- src/com/tdn/model/Admin.java
+++ src/com/tdn/model/Admin.java
@@ -1,5 +1,19 @@
 package com.tdn.model;
 
 public class Admin {
+	private String adminId;
+	private String password;
+	public String getadminId() {
+		return adminId;
+	}
+	public void setadminId(String adminId) {
+		this.adminId = adminId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
