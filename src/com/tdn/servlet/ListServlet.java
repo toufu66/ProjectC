135a9a86a4634f39a7e7f00708926ae8cd1973b7
edu04 <edu04@EDUPC04.n00d01.kis.co.jp>
2020-06-05 10:52:37 +0900

    追記

--- src/com/tdn/servlet/ListServlet.java
+++ src/com/tdn/servlet/ListServlet.java
@@ -16,13 +16,13 @@ import com.tdn.model.User;
  * Servlet implementation class List_Servlet
  */
 @WebServlet("/list")
-public class List_Servlet extends HttpServlet {
+public class ListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public List_Servlet() {
+    public ListServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
