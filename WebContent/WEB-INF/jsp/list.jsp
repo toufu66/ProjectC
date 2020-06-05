9cb8df55a651c49f4bab05190425ef6f2669fc21
edu04 <edu04@EDUPC04.n00d01.kis.co.jp>
2020-06-05 10:35:07 +0900

    追記

---
+++ WebContent/WEB-INF/jsp/list.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+ <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>リスト</title>
+</head>
+<body>
+	<h1>ユーザ一覧</h1>
+
+	<table>
+		<tr>
+			<th>ユーザID</th>
+			<th>名前</th>
+			<th>ふりがな</th>
+		</tr>
+		<c:forEach var="list" items="${list }">
+
+			<tr>
+				<td>${list.uid }</td>
+				<td>${list.name }</td>
+				<td>${list.ruby }</td>
+				<td><a href="delete?sid=${list.uid }">削除</a></td>
+			</tr>
+
+
+		</c:forEach>
+	</table>
+</body>
+</html>
---
+++ src/com/tdn/servlet/List_Servlet.java
@@ -0,0 +1,52 @@
+package com.tdn.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.tdn.model.User;
+
+/**
+ * Servlet implementation class List_Servlet
+ */
+@WebServlet("/list")
+public class List_Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public List_Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		com.tdn.model.UserDAO dao= new com.tdn.model.UserDAO();
+		ArrayList<User> ulist =dao.findUser(null, null, null, null);
+
+		request.setAttribute("list", ulist);
+
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/list.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
