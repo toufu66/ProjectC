8335fe5ed5a8f2f163773c28bb49405f57a6fe0b
edu10 <edu10@EDUPC10.n00d01.kis.co.jp>
2020-06-05 10:16:37 +0900

    仮作成。テスト段階

--- src/com/tdn/servlet/FindServlet.java
+++ src/com/tdn/servlet/FindServlet.java
@@ -1,19 +1,28 @@
 package com.tdn.servlet;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.tdn.model.User;
+import com.tdn.model.UserDAO;
+
 /**
  * Servlet implementation class FindServlet
  */
-@WebServlet("/FindServlet")
+@WebServlet("/find")
 public class FindServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,16 +35,47 @@ public class FindServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher dispatcher=request.getRequestDispatcher("/WEB-INF/jsp/find.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+
+		try {
+			UserDAO udao = new UserDAO();
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+			String name = request.getParameter("name");
+			String ruby = request.getParameter("ruby");
+			String date1Str = request.getParameter("date1");
+			String date2Str = request.getParameter("date2");
+			Timestamp date1 = null;
+			Timestamp date2 = null;
+
+
+			if(date1Str.equals("") != true) {
+				date1 = new Timestamp(sdf.parse(date1Str).getTime());
+			}
+			if(date1Str.equals("") != true) {
+				date2 = new Timestamp(sdf.parse(date2Str).getTime());
+			}
+
+			ArrayList<User> userList = udao.findUser(name,ruby,date1,date2);
+
+			request.setAttribute("list", userList);
+
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/list.jsp");
+			dispatcher.forward(request, response);
+
+			}catch(NumberFormatException | ParseException e){
+				request.setAttribute("ferrormasg", e.getMessage());
+
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/find.jsp");
+				dispatcher.forward(request, response);
+			}
 	}
 
 }
