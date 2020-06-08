696fb52651253636a223756de81dcf911bd4a4a0
edu03 <edu03@EDUPC03.n00d01.kis.co.jp>
2020-06-08 11:07:45 +0900

    とりあえずプッシュ

---
+++ WebContent/WEB-INF/jsp/update.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>変更</title>
+</head>
+<body>
+<h1>変更</h1>
+<form action="update"method="post">
+<input type="hidden" name="uid" value="${user.uid }">
+ユーザ名:<input type="text" name="name" value="${user.name }"><br>
+ふりがな:<input type="text"name="ruby" value="${user.ruby }"><br>
+性別:<input type="text" name="gid" value="${user.gender }"><br>
+生年月日:<input type="date"name="birthday" value="${user.birthday }"><br>
+メールアドレス:<input type="text" name="mail" value="${user.mail }"><br>
+保有ポイント:<input type="text"name="point" value="${user.point }"><br>
+パスワード:<input type="text" name="password" value="${user.pass }"><br>
+区分:<input type="date"name="uclass" value="${user.uclass }"><br>
+<input type="submit"value="送信"><br>
+</form>
+</body>
+</html>
--- src/com/tdn/servlet/UpdateServlet.java
+++ src/com/tdn/servlet/UpdateServlet.java
@@ -1,12 +1,18 @@
 package com.tdn.servlet;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.tdn.model.User;
+import com.tdn.model.UserDAO;
 
 /**
  * Servlet implementation class UpdateServlet
@@ -14,7 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/update")
 public class UpdateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,16 +33,52 @@ public class UpdateServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("UTF-8");
+		String uidStr =request.getParameter("uid");
+		int uid =Integer.parseInt(uidStr);
+
+		UserDAO dao = new UserDAO();
+		User u = dao.findByUid(uid);
+
+		request.setAttribute("user",u);
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/update.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		String uidStr =request.getParameter("uid");
+		int uid =Integer.parseInt(uidStr);
+		String name =request.getParameter("name");
+		String ruby =request.getParameter("ruby");
+		String gidStr =request.getParameter("gid");
+		int gid =Integer.parseInt(gidStr);
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+
+		String birthdayStr = request.getParameter("birthday");
+		Timestamp birthday = null;
+
+
+		String mail =request.getParameter("mail");
+		String pointStr =request.getParameter("point");
+		int point =Integer.parseInt(pointStr);
+		String password =request.getParameter("password");
+		String uClassStr =request.getParameter("uClass");
+		int uClass =Integer.parseInt(uClassStr);
+
+
+
+
+		UserDAO dao = new UserDAO();
+		User u = new User(uid,name,ruby,gid,birthday,mail,point,password,uClass);
+		dao.update(u);
+
+		response.sendRedirect("ulist");
 	}
 
 }
