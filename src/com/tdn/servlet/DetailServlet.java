487a4b3df7d07dc251356562d2e9151190e99d93
edu01 <edu01@EDUPC01.n00d01.kis.co.jp>
2020-06-09 10:51:09 +0900

    ログイン状態必須

--- src/com/tdn/servlet/DetailServlet.java
+++ src/com/tdn/servlet/DetailServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.tdn.model.User;
 import com.tdn.model.UserDAO;
@@ -32,22 +33,29 @@ public class DetailServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session=request.getSession();
+		String admin = (String) session.getAttribute("admin");
+		if(admin != null) {
+			request.setCharacterEncoding("UTF-8");
+			String uidStr=request.getParameter("uid");
+			int uid=Integer.parseInt(uidStr);
 
-		request.setCharacterEncoding("UTF-8");
-		String uidStr=request.getParameter("uid");
-		int uid=Integer.parseInt(uidStr);
+			UserDAO udao =new UserDAO();
+			User u =udao.findByUid(uid);
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			String userBirthday = sdf.format(u.getBirthday());
 
-		UserDAO udao =new UserDAO();
-		User u =udao.findByUid(uid);
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		String userBirthday = sdf.format(u.getBirthday());
-
-		request.setAttribute("user",u);
-		request.setAttribute("birthday", userBirthday);
+			request.setAttribute("user",u);
+			request.setAttribute("birthday", userBirthday);
 
 
-		RequestDispatcher dispatcher=request.getRequestDispatcher("/WEB-INF/jsp/detail.jsp");
-		dispatcher.forward(request, response);
+			RequestDispatcher dispatcher=request.getRequestDispatcher("/WEB-INF/jsp/detail.jsp");
+			dispatcher.forward(request, response);
+		}else {
+			String RedirectUrl = "login";
+			response.sendRedirect(RedirectUrl);
+
+		}
 	}
 
 	/**
